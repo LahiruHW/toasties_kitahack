52adc41a14c0d7df277629a8979a5fabee965c61
Lahiru Weliwitiya <lahiruweliwitiya@hotmail.com>
2023-12-30 19:17:14 +0800

    remove empty lines in theme data file

--- toasties_flutter/lib/toasties_theme.dart
+++ toasties_flutter/lib/toasties_theme.dart
@@ -92,14 +92,25 @@ class ToastiesTextTheme {
   );
 
   static TextTheme globalTextTheme = TextTheme(
-
-
-
+    headlineLarge: ToastiesTextTheme.headingStyle.copyWith(fontSize: 30),
+    headlineMedium: ToastiesTextTheme.headingStyle.copyWith(fontSize: 26),
+    headlineSmall: ToastiesTextTheme.headingStyle.copyWith(fontSize: 22),
+    ///////////////////////////////////////////////////////////////////////
+    displayLarge: ToastiesTextTheme.headingStyle.copyWith(fontSize: 30),
+    displayMedium: ToastiesTextTheme.headingStyle.copyWith(fontSize: 26),
+    displaySmall: ToastiesTextTheme.headingStyle.copyWith(fontSize: 22),
+    ///////////////////////////////////////////////////////////////////////
+    bodyLarge: ToastiesTextTheme.bodyStyle.copyWith(fontSize: 20),
+    bodyMedium: ToastiesTextTheme.bodyStyle.copyWith(fontSize: 18),
+    bodySmall: ToastiesTextTheme.bodyStyle.copyWith(fontSize: 16),
+    ///////////////////////////////////////////////////////////////////////
+    titleLarge: ToastiesTextTheme.logoStyle.copyWith(fontSize: 30),
+    titleMedium: ToastiesTextTheme.logoStyle.copyWith(fontSize: 26),
+    titleSmall: ToastiesTextTheme.logoStyle.copyWith(fontSize: 22),
+    ///////////////////////////////////////////////////////////////////////
     labelLarge: ToastiesTextTheme.labelStyle.copyWith(fontSize: 20),
     labelMedium: ToastiesTextTheme.labelStyle.copyWith(fontSize: 18),
     labelSmall: ToastiesTextTheme.labelStyle.copyWith(fontSize: 16),
-
-    
-
   );
+
 }
