7261a5c7744a54c9afb46142ce96fb9366ee7ddd
Lahiru Weliwitiya <lahiruweliwitiya@hotmail.com>
2024-01-04 05:22:55 +0530

    add bottom nav bar utility widget

---
+++ toasties_flutter/lib/common/widgets/bottom_nav_bar.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class ToastiesBottomNavBar extends StatefulWidget {
+  const ToastiesBottomNavBar({
+    super.key,
+    this.index,
+  });
+
+  final int? index;
+
+  @override
+  State<ToastiesBottomNavBar> createState() => _ToastiesBottomNavBarState();
+}
+
+class _ToastiesBottomNavBarState extends State<ToastiesBottomNavBar> {
+  late int selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedIndex = widget.index ?? 0;
+  }
+
+  void _onTapped(int index) {
+    setState(() => selectedIndex = index);
+
+    switch (index) {
+      case 0:
+        GoRouter.of(context).go('/home');
+        break;
+      case 1:
+        GoRouter.of(context).go('/chat');
+        break;
+      case 2:
+        GoRouter.of(context).go('/saved');
+        break;
+      case 3:
+        GoRouter.of(context).go('/profile');
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      elevation: 1,
+      currentIndex: selectedIndex,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          activeIcon: Icon(Icons.home_sharp),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.chat_outlined),
+          activeIcon: Icon(Icons.chat_rounded),
+          label: 'Chat',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.bookmark_border_rounded),
+          activeIcon: Icon(Icons.bookmark_rounded),
+          label: 'Saved',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline_rounded),
+          activeIcon: Icon(Icons.person_rounded),
+          label: 'Profile',
+        ),
+      ],
+      onTap: _onTapped,
+    );
+  }
+}
