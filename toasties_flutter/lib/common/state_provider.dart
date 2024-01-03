06795d8313131a7a88e1699bcb72755ad0785f7f
Lahiru Weliwitiya <lahiruweliwitiya@hotmail.com>
2024-01-04 05:22:25 +0530

    allow settings modification via provider

--- toasties_flutter/lib/common/state_provider.dart
+++ toasties_flutter/lib/common/state_provider.dart
@@ -1,17 +1,28 @@
 
 import 'package:flutter/material.dart';
+import 'package:toasties_flutter/common/entity/settings.dart';
 
 // contains the state of the app
 class ToastieStateProvider extends ChangeNotifier {
 
+  late Settings settings;
 
-  ToastieStateProvider();
-
-  void incrementCounter() {
-    notifyListeners();
+  ToastieStateProvider(){
+    // initialize the state of the app
+    settings = Settings(
+      isDarkMode: false,
+    );
+    debugPrint('------------------------------ ToastieStateProvider initialized');
   }
 
-
+  void updateSettings({
+    bool? isDarkMode,
+  }) {
+    settings.updateSettings(
+      isDarkMode: isDarkMode,
+    );
+    notifyListeners();
+  }
 
 }
 
