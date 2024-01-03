e663735dbcccf749f915a3133681f63064369649
Mizore66 <123628501+Mizore66@users.noreply.github.com>
2024-01-04 02:31:38 +0800

    Prompt engineering done

--- myfunnyGenAi/chatbot.py
+++ myfunnyGenAi/chatbot.py
@@ -21,7 +21,13 @@ def add_model_response(conversation, model):
 if __name__ == "__main__":
     model_gemini_pro = initialise()
 
-    conversation = [construct_message('Can you write a 50 word essay on cats?')]
+    prompt_start = "Answer this question as the financial lawyer named LAILA:\n\n"
+
+    inital_prompt = """Act like you are a financial lawyer named LAILA (Legally & Artificially Intelligent Lawyer Assistant) who is here to help me with any problems I have with financial law. Can you please give a short introduction of yourself to me?"""
+
+    prompt_end = "\n\nPlease answer this question with the personality of how a good, friendly financial lawyer would with me, ask me for more context if the question doesn't provide enough of it"
+
+    conversation = [construct_message(inital_prompt)]
 
     while True:
         add_model_response(conversation, model_gemini_pro)
@@ -29,4 +35,5 @@ if __name__ == "__main__":
         print('---'*15)
         if user_input.lower == "quit":
             break
-        conversation.append(construct_message(user_input))
+        prompt = prompt_start + user_input + prompt_end
+        conversation.append(construct_message(prompt))
